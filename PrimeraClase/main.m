62186f6c2a20e95599c5aa40a8e28e77f0ec9c97
José Gatica Altamirano <josegaticaaltamirano@MacBook-de-Jose.local>
2014-11-06 16:11:52 -0300

    Initial Commit

---
+++ PrimeraClase/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PrimeraClase
+//
+//  Created by José Gatica Altamirano on 06/11/14.
+//  Copyright (c) 2014 guitarjosegatica. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
